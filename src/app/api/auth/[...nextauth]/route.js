5992a71fbd3379328cfbcb96b1eaea42924fb0fe
Arpan <rockarpan31@gmail.com>
2024-05-25 21:39:54 +0545

    ok

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -2,8 +2,8 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
-//import { FirestoreAdapter } from "@auth/firebase-adapter"
-//import { cert } from "firebase-admin/app";
+// import { FirestoreAdapter } from "@auth/firebase-adapter"
+// import { cert } from "firebase-admin/app";
 
 export const authOptions = {
     // adapter: FirestoreAdapter({
