7a4175ef122a009371b4cd81c05cf74a354f0e58
Arpan <rockarpan31@gmail.com>
2024-07-01 18:35:50 +0545

    ok

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -67,7 +67,7 @@ function Page() {
           <h2 className="text-3xl font-bold tracking-tight">
             Hi, Welcome back {userData && userData.fullName ? userData.fullName : ''} 🫡
 
-            <Overnmbhjvuiview/>
+            <Overview />
 
           </h2>
         )}
