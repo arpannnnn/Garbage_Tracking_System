1127ba9ddf2a557ffdb2796d7e025d8ab0704472
Arpan <rockarpan31@gmail.com>
2024-07-14 19:13:03 +0545

    ok

--- components/SmartDustbin.jsx
+++ components/SmartDustbin.jsx
@@ -228,7 +228,7 @@ const SmartDustbin = () => {
 
         const setDailyNotification = () => {
             const now = new Date();
-            const notificationTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 14, 40, 0);
+            const notificationTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 14, 42, 0);
 
             if (now > notificationTime) {
                 notificationTime.setDate(notificationTime.getDate() + 1);
